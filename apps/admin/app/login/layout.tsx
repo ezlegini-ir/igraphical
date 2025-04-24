aa99695a30283af5ff2041264dfebc014cdfe044
Alireza Ezlegini <alireza.ezlegini@yahoo.com>
2025-04-24 03:43:45 +0330

    Add recaptchaKey prop to RecaptchaWrapper in layout components

--- apps/admin/app/login/layout.tsx
+++ apps/admin/app/login/layout.tsx
@@ -7,7 +7,11 @@ export default function layout({ children }: { children: ReactNode }) {
       className={`max-w-screen-xl mx-auto p-3 grid grid-cols-1 grid-rows-[auto_1fr_auto] min-h-screen`}
     >
       <main className="mt-24 w-full max-w-[350px] mx-auto">
-        <RecaptchaWrapper>{children}</RecaptchaWrapper>
+        <RecaptchaWrapper
+          recaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+        >
+          {children}
+        </RecaptchaWrapper>
       </main>
     </div>
   );
--- apps/tutor/app/login/layout.tsx
+++ apps/tutor/app/login/layout.tsx
@@ -12,7 +12,11 @@ export default async function layout({ children }: { children: ReactNode }) {
       className={`max-w-screen-xl mx-auto p-3 grid grid-cols-1 grid-rows-[auto_1fr_auto] min-h-screen`}
     >
       <main className="mt-24 w-full max-w-[350px] mx-auto">
-        <RecaptchaWrapper>{children}</RecaptchaWrapper>
+        <RecaptchaWrapper
+          recaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+        >
+          {children}
+        </RecaptchaWrapper>
       </main>
     </div>
   );
