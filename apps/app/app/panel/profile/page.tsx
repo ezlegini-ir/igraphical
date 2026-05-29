d55a2b6ae643ca2f49baf8084581c2d2ae75d9cf
Alireza Ezlegini <alireza.ezlegini@yahoo.com>
2026-05-29 22:03:24 +0330

    Bring back profile form

--- apps/app/app/panel/profile/page.tsx
+++ apps/app/app/panel/profile/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import UserProfileForm from "@/components/forms/profileForm";
-// import { getSessionUser } from "@/data/user";
+import { getSessionUser } from "@/data/user";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { loginPageRoute } from "@/middleware";
 
 const page = async () => {
-  // const user = await getSessionUser();
+  const user = await getSessionUser();
 
-  return <UserProfileForm />;
+  if (!user) redirect(loginPageRoute);
+
+  return <UserProfileForm user={user} />;
 };
 
 export default page;
